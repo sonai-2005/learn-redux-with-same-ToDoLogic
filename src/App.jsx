84c76792d022d578714fbd1a5a8bb1747f85aa44
sonai-2005 <nandisonai60@gmail.com>
2025-03-30 03:26:16 +0530

    add the yml file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/todos'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo/>
+      <Todos/>
+      
+    </>
+  )
+}
+
+export default App
